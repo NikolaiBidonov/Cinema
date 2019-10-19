c2872629c3382fc64831dfb0d9d5aa75c12f4b4a
NikolaiBidonov <bidonovy@mail.ru>
2019-10-19 13:10:42 +0300

    4.4-2-Добавлены ссылки к фильмам

--- js/films.js
+++ js/films.js
@@ -1,48 +1,58 @@
-
 let genres = ['фантастика', 'приключения', 
 'боевик', 'фэнтези', 'комедия', 'драма', 'мультфильм'];
-
+let site = 'https://www.kinopoisk.ru/film/'
 let films = [
-    ['10:00', 'Человек-паук', genres[0] + ', ' + genres[1] + ', ' + genres[2]],
-    ['12:00', 'Собачья жизнь 2', genres[3] + ', ' + genres[5] + ', ' + genres[4]],
-    ['14:00', 'История игрушек 4', genres[6] + ', ' + genres[1] + ', ' + genres[4]],
-    ['16:00', 'Люди в черном: Интернэшнл', genres[3] + ', ' + genres[2] + ', ' + genres[1]],
+    {start : '10:00', 
+    name: 'Человек-паук',
+    genre: genres[0] + ', ' + genres[1] + ', ' + genres[2],
+    siteLink: site + 'chelovek-pauk-2002-838/',
+    },
+    {start: '12:00',
+    name: 'Собачья жизнь 2',
+    genre: genres[3] + ', ' + genres[5] + ', ' + genres[4],
+    siteLink: site + '1122114/',
+    },
+    {start : '14:00', 
+    name: 'История игрушек 4',
+    genre: genres[6] + ', ' + genres[1] + ', ' + genres[4],
+    siteLink: site + '846824/',
+    },
+    {start: '16:00',
+    name:  'Люди в черном: Интернэшнл',
+    genre: genres[3] + ', ' + genres[2] + ', ' + genres[1],
+    siteLink: site + '693730/',
+    },
 ]
-
-
-
-const elBody = document.getElementById('tableBody');
+const svg = document.getElementById('svgPlus')
+let elBody = document.getElementById('tableBody');
 for (var i=0; i<films.length; i++) {
     
-    const elRow = document.createElement('tr')
-    const start = document.createElement('td')
-    start.innerText = films[i][0]
-
-    const link = document.createElement('a')
-    link.href = 'https://www.kinopoisk.ru/film/chelovek-pauk-2002-838/'
-    link.title = 'Кинопоиск'
-    link.target= '_blank'
-    link.appendChild(document.createTextNode(films[i][1]))
-
-    const name = document.createElement('td')
-    const genre = document.createElement('td')
-    genre.innerText = films [i][2]
-
-    const plus = document.createElement('td')
-    const container = document.createElement('div')
     
-    plus.appendChild(container)
-    name.appendChild(link)
-    elRow.appendChild(start)
-    elRow.appendChild(name)
-    elRow.appendChild(genre)
-    elRow.appendChild(plus)
-    elBody.appendChild(elRow)
+    const elRow = document.createElement('tr');
+    const start = document.createElement('td');
+    start.innerText = films[i].start;
+
+    const link = document.createElement('a');
+    link.href = films[i].siteLink;
+    link.title = 'Кинопоиск';
+    link.target= '_blank';
+    link.appendChild(document.createTextNode(films[i].name));
+
+    const name = document.createElement('td');
+    const genre = document.createElement('td');
+    genre.innerText = films[i].genre;
+
+    const plus = document.createElement('td');
+    
+    name.appendChild(link);
+    elRow.appendChild(start);
+    elRow.appendChild(name);
+    elRow.appendChild(genre);
+    elRow.appendChild(plus);
+    elBody.appendChild(elRow);
+    plus.appendChild(svg.content.cloneNode(true))
     start.classList.add('col1');
     name.classList.add('col2');
-    genre.classList.add('col3')
-    container.classList.add('svgImage')
-    container.classList.add('col4')
-    
-
+    genre.classList.add('col3');
+    plus.classList.add('col4');
 }
