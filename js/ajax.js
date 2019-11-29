6033b348b71cd0a709847c17da9001ea56cb5156
NikolaiBidonov <bidonovy@mail.ru>
2019-11-29 11:42:44 +0300

    7.3-1-Добавлен новый функционал: определение города по ip и возможность изменения города. Реализовано с помощью ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,22 +1,86 @@
-const sypex_url = 'https://api.sypexgeo.net/'
+const SYPEX_URL = 'https://api.sypexgeo.net/'
+const GLAV_URL = 'https://glavpunkt.ru/api/get_rf_cities'
 
-function getRequest(url, callback){
-    let xhr = new XMLHttpRequest();
-    let async = true;
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback.call(xhr.responseText)
-        }
+
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: SYPEX_URL,
+    success: function (data) {
+        let city = data['city']['name_ru'];
+        $('#cityModal').animate({
+            top: '5rem',
+            opacity: 1
+        }, 1500);
+        $('#city-name').html(city);
+        $('#formCity').html(city);
+    },
+    error: function () {
+        $('#changeCityModal').animate({
+            right: '6rem'
+        }, 300);
+        getCitiesByApi();
     }
-    xhr.open("GET", url, async);
-    xhr.send();
-    return(xhr.responseText);
+})
+
+let cities;
+
+$('#cityYes').on('click', function () {
+    $('#cityModal').animate({
+        opacity: 0
+    }, 1000)
+
+})
+
+function getCitiesByApi () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: GLAV_URL,
+        success: function (data) {
+            cities = data;
+            $('#inputCity').keyup(function () {
+                let search = $(this).val();
+                let count = 0;
+                let html = '<ul>';
+                for (let i = 0; i < cities.length; i++) {
+                    if (cities[i].name.toLowerCase().indexOf(search.toLowerCase()) >= 0 && count <= 5) {
+                        html += '<li data-city="' + cities[i].name + '">' + cities[i].name + ' (' + cities[i].area + ')</li>'
+                        count++;
+                    }
+                }
+                html += '</ul>';
+                $('#result').html(html);
+            })
+        }
+    })
+    
 }
 
-$(document).ready(function() {
-    getRequest(sypex_url, function() {
-        let answer = $.parseJSON(this);
-        let city = answer['city']['name_ru'];
-        $('#city-name').html(city);
+$(document).ready(function () {
+    $('#cityNo').on('click', function () {
+        $('#cityModal').animate({
+            right: '-26rem'
+        }, 300)
+        $('#changeCityModal').animate({
+            right: '6rem'
+        }, 300)
+        if (!cities) {
+            getCitiesByApi();
+        }
     })
+})
+
+$('body').on('click', '#result li', function () {
+    $('#city-name').html($(this).data('city'));
+    $('#changeCityModal').animate({
+        right: '-26rem'
+    }, 300);
+})
+
+$('#city-name').on('click', function () {
+    $('#changeCityModal').animate({
+        right: '6rem'
+    }, 300)
+    getCitiesByApi();
 })
